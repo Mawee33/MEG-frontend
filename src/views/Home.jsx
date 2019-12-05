515037b48f80b1cba87e74d81832087db44f4ff6
VÉRONIQUE GONZALEZ <vero1308@mac.comgit config --global user.name Your>
2019-12-05 12:10:14 +0100

    home2

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -4,24 +4,18 @@ import React from "react";
 const Home = () => {
   return (
     <div className="firstView">
-      <h1>LOOKBOOK</h1>
-      <h4>Découvrez notre selection</h4>
-      <div className="slideshow">
-        <ul className="Content">
-          <li>
-            <img
-              className="intro"
-              src="https://res.cloudinary.com/dfnnpxhx9/image/upload/v1575532226/MEG/fashion-1031469_1920_ybijwc.jpg"
-              alt="dressing"
-            />
-          </li>
-          <li>
-            <img src="" alt="" />
-          </li>
-          <li>
-            <img src="" alt="" />
-          </li>
-        </ul>
+      <header>
+        {/* <img
+        className="intro"
+        src="https://res.cloudinary.com/dfnnpxhx9/image/upload/v1575532226/MEG/fashion-1031469_1920_ybijwc.jpg"
+        alt="dressing"
+      /> */}
+        <h1>LOOKBOOK</h1>
+        <h3>Découvrez notre selection</h3>
+      </header>
+      <div className="transistion">
+        <h2>SUIVEZ-NOUS</h2>
+        <h4>#lesminettesengoguette</h4>
       </div>
     </div>
   );
