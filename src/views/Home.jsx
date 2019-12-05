25b18aabd88f5e175940c74439a9809c99874190
VÉRONIQUE GONZALEZ <vero1308@mac.comgit config --global user.name Your>
2019-12-05 09:36:24 +0100

    cloudinary creation

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,9 +1,23 @@
 import React from "react";
+const cloudinary = require("./../config/cloudinary");
 
 const Home = () => {
   return (
     <div>
       <h1>Home Page</h1>
+      <div className="slideshow">
+        <ul className="Content">
+          <li>
+            <img src="" alt="" />
+          </li>
+          <li>
+            <img src="" alt="" />
+          </li>
+          <li>
+            <img src="" alt="" />
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
