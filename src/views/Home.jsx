f90afb736aad54ce9aa160fd1774f2af4d7c8244
VÉRONIQUE GONZALEZ <vero1308@mac.comgit config --global user.name Your>
2019-12-05 13:32:22 +0100

    home3

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -13,10 +13,56 @@ const Home = () => {
         <h1>LOOKBOOK</h1>
         <h3>Découvrez notre selection</h3>
       </header>
-      <div className="transistion">
+      <div className="transition">
         <h2>SUIVEZ-NOUS</h2>
         <h4>#lesminettesengoguette</h4>
       </div>
+      <section id="informations">
+        <div class="informations">
+          <div class="infoImg">
+            <img
+              src="https://res.cloudinary.com/dfnnpxhx9/image/upload/v1575532200/MEG/people-2589862_1920_cocya2.jpg"
+              alt="Nos produits"
+            />
+          </div>
+          <div class="small-info">
+            <h4>Produits</h4>
+            <p>Venez découvrir notre gamme de vêtements pour toutes</p>
+          </div>
+        </div>
+
+        <div class="informations">
+          <div class="infoImg">
+            <img
+              src="https://res.cloudinary.com/dfnnpxhx9/image/upload/v1575530772/MEG/IMG_3104_upjjt4.jpg"
+              alt="La fabrication"
+            />
+          </div>
+          <div class="small-info">
+            <h4>Fabrication</h4>
+            <p>
+              Une production réalisée en France, avec des tissus fabriqués à
+              base de fils issus de la porduction bilogique.
+            </p>
+          </div>
+        </div>
+
+        <div class="informations">
+          <div class="infoImg">
+            <img
+              src="https://res.cloudinary.com/dfnnpxhx9/image/upload/v1575530772/MEG/IMG_3105_zhsliy.jpg"
+              alt="mon histoire"
+            />
+          </div>
+          <div class="small-info">
+            <h4>Histoire</h4>
+            <p>
+              Tout part d'une envie de positiver et de profiter du savoir-faire
+              français du monde la Mode.
+            </p>
+          </div>
+        </div>
+      </section>
     </div>
   );
 };
