81a043b36b59e1d62c55a3286682451883be76ec
VÉRONIQUE GONZALEZ <vero1308@mac.comgit config --global user.name Your>
2019-12-05 10:37:55 +0100

    img

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -8,7 +8,10 @@ const Home = () => {
       <div className="slideshow">
         <ul className="Content">
           <li>
-            <img src="" alt="" />
+            <img
+              src="https://res.cloudinary.com/dfnnpxhx9/image/upload/v1575532226/MEG/fashion-1031469_1920_ybijwc.jpg"
+              alt="dressing"
+            />
           </li>
           <li>
             <img src="" alt="" />
