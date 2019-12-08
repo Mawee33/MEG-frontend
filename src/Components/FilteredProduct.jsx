dbfd902b5ef876ee5fc54b0a856371e7ffa3a8d4
Mawee33 <mariegreiner33@gmail.com>
2019-12-08 15:14:59 +0100

    filteredProduct

--- src/Components/FilteredProduct.jsx
+++ src/Components/FilteredProduct.jsx
@@ -1,31 +1,32 @@
 import React, { Component} from "react";
+import axios from "axios";
 import Vetement from "./views/Vetement";
 import SearchBar from "./SearchBar";
 
-export default class FilteredProduct extends Component {
-state = {vetements: [...this.props.vetements] };
+const FilteredProduct = () => {
+// state = {vetements: [...this.props.vetements] };
 
-favoriteProducts = e => {
-  const searchQuery = e.target.value;
-}
-  this.setState({ products: favoriteProducts });
+// favoriteProducts = e => {
+//   const searchQuery = e.target.value;
+// }
+//   this.setState({ products: favoriteProducts });
 
-  render() {
+//   render() {
 
   return (
     <div>
-      <p>Affinez votre recherche</p>
-      <table className="table">
-        {this.props.vetements.map((v, i) => (
-          <SearchBar clbk={this.favoriteProducts} />
-          <Vetement 
-          key={i}
-          name={v.name}
-          image={v.image}
-          description={v.description}
-          size={v.size}
-          color={v.color} />
-        ))}
+    <p>Affinez votre recherche</p>
+    <table className="table">
+     {/* {this.props.vetements.map((v, i) => ( */}
+    {/* //       <SearchBar clbk={this.favoriteProducts} />
+    //       <Vetement  */}
+    {/* //       key={i}
+    //       name={v.name}
+    //       image={v.image}
+    //       description={v.description}
+    //       size={v.size}
+    //       color={v.color} />
+    //     ))} */}
         {/* // <thead>
         //   <tr>
                <th>Nom</th>
